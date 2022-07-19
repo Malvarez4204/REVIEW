12c5a084b8e32ca954e16242f5a86a6a549ff5d3
Malvarez4204 <106292326+Malvarez4204@users.noreply.github.com>
2022-07-18 20:32:07 -0500

    REVIEW

---
+++ Review/Review/ContentView.swift
@@ -0,0 +1,29 @@
+//
+//  ContentView.swift
+//  Review
+//
+//  Created by MAC on 30/06/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+        Button(/*@START_MENU_TOKEN@*/"Button"/*@END_MENU_TOKEN@*/) {
+            
+        }
+
+        Button("Preview") {
+            /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+            .previewInterfaceOrientation(.landscapeRight)
+    }
+}
---
+++ Review/Review/ReviewApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReviewApp.swift
+//  Review
+//
+//  Created by MAC on 30/06/22.
+//
+
+import SwiftUI
+
+@main
+struct ReviewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
